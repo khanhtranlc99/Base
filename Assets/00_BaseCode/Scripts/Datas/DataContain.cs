25afe2e48231fdd216583dcd350fffc691e78849
trankhanhlc <trankhanhlc99@gmail.com>
2023-04-08 15:08:40 +0700

    Base code

---
+++ Assets/00_BaseCode/Scripts/Datas/DataContain.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataContain : MonoBehaviour
+{
+    public GiftDatabase giftDatabase;
+    public LevelChestData levelChestData;
+    public StarChestData starChestData;
+    public BirdChestData birdChestData;
+
+}
